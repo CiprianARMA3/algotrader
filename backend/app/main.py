9dd74e6154a091bfdde0c236393898aa4dda38e1
ciprianwindows <stefanoepictyytb@gmail.com>
2026-01-05 18:51:08 +0100

    fine

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 import uvicorn
+import os
 from app.api.endpoints import router as api_router
 from app.core.config import settings
 import logging
